524e045c11048853667830223b8aafcada3f8afc
Мухаммад <human.twm@gmail.com>
2021-01-31 22:14:04 +0300

    Breakpoint/Part1-CardsAndCourse

--- ptechml/ptechml/settings.py
+++ ptechml/ptechml/settings.py
@@ -21,9 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'k(5u546zrhk2=d@6+ptljt**9trhv=5(uj*9%kg28miqp*)r6u'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
